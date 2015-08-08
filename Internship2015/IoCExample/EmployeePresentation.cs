292ecde5264af6571fb9dc7195aa53a8f89e6692
Tia94 <patricia.atieyeh@gmail.com>
2015-08-08 12:55:37 +0300

    added unity nuget package

---
+++ Internship2015/IoCExample/EmployeePresentation.cs
@@ -0,0 +1,29 @@
+﻿using IoCExample.Bll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoCExample
+{
+    class EmployeePresentation
+    {
+        private IEmployeeBll _bll;
+ 
+        public EmployeePresentation(IEmployeeBll bll)
+        {
+            _bll = bll;
+        }
+
+        public void ShowEmployeesOnConsole()
+        {
+            var list = _bll.GetEmployees();
+
+            foreach(var item in list)
+            {
+                Console.WriteLine("Name: {0}, Salary: {1}, Address: {2}", item.Name, item.Salary, item.Address);
+            }
+        }
+    }
+}
---
+++ Internship2015/IoCExample/FilePath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoCExample
+{
+    class FilePath : IFilePath
+    {
+        public string Path { get; set; }
+
+        public FilePath(string path)
+        {
+            Path = path;
+        }
+    }
+}
