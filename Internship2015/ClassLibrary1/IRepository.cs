0acc906079a401853cb8ae856385398bc25abfae
Tia94 <patricia.atieyeh@gmail.com>
2015-08-31 09:55:20 +0300

    hjgj

---
+++ Internship2015/ClassLibrary1/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IoCExample.Domain;
+
+
+namespace ClassLibrary1
+{
+    public interface IRepository
+    {
+        void Save<TEntity>(TEntity entity) where TEntity : Entity;
+    }
+}
