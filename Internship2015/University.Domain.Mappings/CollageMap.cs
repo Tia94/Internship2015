803d75a3cdd854983207f96e79b0c320468cd9bc
Sp1n89 <s.snake90@hotmail.com>
2015-08-31 08:42:24 +0300

    Added domain

---
+++ Internship2015/University.Domain.Mappings/CollageMap.cs
@@ -0,0 +1,22 @@
+﻿
+using FluentNHibernate.Mapping;
+
+
+namespace University.Domain.Mappings
+{
+    public class CollageMap :ClassMap<Collage>
+    {
+        public CollageMap()
+        {
+            Table("Collage");
+            Id(x => x.Id).GeneratedBy.Identity();
+
+            Map(x => x.Name).Not.Nullable();
+            Map(x => x.Address);
+
+            HasManyToMany(x => x.Courses).Table("CollageCourse").ParentKeyColumn("CollageID").ChildKeyColumn("CourseID");
+
+
+        }
+    }
+}
---
+++ Internship2015/University.Domain/Professor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace University.Domain
+{
+    public class Professor : Entity
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public IList<Course> Courses { get; protected set; }
+
+        public Professor()
+        {
+            Courses = new List<Course>();
+        }
+    }
+}
